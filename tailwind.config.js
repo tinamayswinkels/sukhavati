a14011c47b02e86759f315b8f53b67b39beb80a4
dezoul <aussieme@gmail.com>
2024-11-26 12:16:01 +0800

    fixed font

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,13 +22,6 @@ module.exports = {
       '8xl': ['6rem', '1'],
       '9xl': ['8rem', '1'],
     },
-    fontFamily: {
-      'sans': ['proxima-nova', 'ui-sans-serif'],
-      'sans-tight': ['proxima-nova-condensed', 'ui-sans-serif'],
-      'mono': ['Inconsolata', 'ui-mono', 'monospace'],
-      'soft': ['proxima-soft', 'ui-sans-serif'],
-      'soft-tight': ['proxima-soft-condensed', 'ui-sans-serif'],
-    },
     extend: {
       lineHeight: {
         'tight': '.85',
